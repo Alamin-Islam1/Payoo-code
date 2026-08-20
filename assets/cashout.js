fb377d5ea3a4a3edb34e306f57e65edc3edcafb7
mdalaminhossain193750-create <md.alaminhossain193750@gmail.com>
2026-08-20 06:05:20 +0600

    thrid commit

--- assets/cashout.js
+++ assets/cashout.js
@@ -1,48 +1,86 @@
 
 document.getElementById("input-btn").addEventListener("click", function(){
-// 1 : get the agent number &validate 
-const inputAgent = document.getElementById("input-agent");
-const newAgent = inputAgent.value ;
-console.log(newAgent);
-if(newAgent.length !=11){
+// 1 : get the agent number
+const inputAgent = getValueFromInput("input-agent");
+console.log(inputAgent);
+if(inputAgent.length !=11){
     alert("Invalid Agent number");
     return;
 }
-
-// 2 : get the amount , validate ,convert to number
-const inputAmount = document.getElementById("input-amount");
-const newAmount = inputAmount.value ;
-console.log(newAmount);
-
+// 2 : get the amount
+const inputAmount =getValueFromInput("input-amount");
 // 3 : get the urrent Balance , validate ,convert to number
 const inputBalance = document.getElementById("balance");
 const balance = inputBalance.innerText ;
 console.log(balance);
-
 // 4 : Calculate new Balance
-const newBalance = Number(balance) - Number(newAmount) ;
+const newBalance = Number(balance) - Number(inputAmount) ;
+console.log(newBalance);
 if(newBalance < 0){
     alert("Invalid Amount")
     return;
 }
-
-
-//5 : Get the pin and verify 
-const inputPassword = document.getElementById("input-password");
-const pin = inputPassword.value ;
+const pin =getValueFromInput("input-password");
 if(pin ==="1937"){
 
-    // 5-1 : true :: show an alert > set Balance
-    alert("cashout successfull");
+ alert("cashout successfull");
     console.log("new balance", newBalance);
     
     inputBalance.innerText =newBalance ; 
-}else{
+}
+else{
 
-    // 5-2 true :: show anerror alert > return
-    alert("Invalid pin");
+alert("Invalid pin");
     return;
 }
+})
+
+
+
+// document.getElementById("input-btn").addEventListener("click", function(){
+// // 1 : get the agent number &validate 
+// const inputAgent = document.getElementById("input-agent");
+// const newAgent = inputAgent.value ;
+// console.log(newAgent);
+// if(newAgent.length !=11){
+//     alert("Invalid Agent number");
+//     return;
+// }
+
+// // 2 : get the amount , validate ,convert to number
+// const inputAmount = document.getElementById("input-amount");
+// const newAmount = inputAmount.value ;
+// console.log(newAmount);
+
+// // 3 : get the urrent Balance , validate ,convert to number
+// const inputBalance = document.getElementById("balance");
+// const balance = inputBalance.innerText ;
+// console.log(balance);
+
+// // 4 : Calculate new Balance
+// const newBalance = Number(balance) - Number(newAmount) ;
+// if(newBalance < 0){
+//     alert("Invalid Amount")
+//     return;
+// }
+
+
+// //5 : Get the pin and verify 
+// const inputPassword = document.getElementById("input-password");
+// const pin = inputPassword.value ;
+// if(pin ==="1937"){
+
+//     // 5-1 : true :: show an alert > set Balance
+//     alert("cashout successfull");
+//     console.log("new balance", newBalance);
+    
+//     inputBalance.innerText =newBalance ; 
+// }else{
+
+//     // 5-2 true :: show anerror alert > return
+//     alert("Invalid pin");
+//     return;
+// }
 
     
-})
+// })
